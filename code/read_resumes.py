483d2e3063497333ec7acb25ea1b6c865f02b832
shravanmateti <shravanmateti@gmail.com>
2020-09-11 15:01:35 +0530

    9/11 update

---
+++ code/read_resumes.py
@@ -0,0 +1,123 @@
+#Working directory
+print("Current Working Directory " , os.getcwd())
+myPath=os.chdir("D:/BITS MTech/4-sem/Project/Dupe check/Data sets/Resume&Job_Description/dupe-check/dataset")
+
+
+import glob
+import os
+import warnings
+import textract
+import requests
+from flask import (Flask, json, Blueprint, jsonify, redirect, render_template, request,
+                   url_for)
+from gensim.summarization import summarize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+from werkzeug import secure_filename
+
+#import pdf2text as pdf
+import PyPDF2
+import pandas as pd
+
+
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+
+class ResultElement:
+    def __init__(self, rank, filename):
+        self.rank = rank
+        self.filename = filename
+
+
+def getfilepath(loc):
+    temp = str(loc)
+    temp = temp.replace('\\', '/')
+    return temp
+
+
+def res(jobfile):
+    Resume_Vector = []
+    Ordered_list_Resume = []
+    Ordered_list_Resume_Score = []
+    LIST_OF_FILES = []
+    LIST_OF_FILES_PDF = []
+    LIST_OF_FILES_DOC = []
+    LIST_OF_FILES_DOCX = []
+    Resumes = []
+    Temp_pdf = []
+    os.chdir("D:/BITS MTech/4-sem/Project/Dupe check/Data sets/Resume&Job_Description/dupe-check/dataset")
+    for file in glob.glob('**/*.pdf', recursive=True):
+        LIST_OF_FILES_PDF.append(file)
+    for file in glob.glob('**/*.doc', recursive=True):
+        LIST_OF_FILES_DOC.append(file)
+    for file in glob.glob('**/*.docx', recursive=True):
+        LIST_OF_FILES_DOCX.append(file)
+
+    LIST_OF_FILES = LIST_OF_FILES_DOC + LIST_OF_FILES_DOCX + LIST_OF_FILES_PDF
+    # LIST_OF_FILES.remove("antiword.exe")
+    print("This is LIST OF FILES")
+    print(LIST_OF_FILES)
+
+    # print("Total Files to Parse\t" , len(LIST_OF_PDF_FILES))
+    print("####### PARSING ########")
+    for nooo,i in enumerate(LIST_OF_FILES):
+        Ordered_list_Resume.append(i)
+        Temp = i.split(".")
+        if Temp[1] == "pdf" or Temp[1] == "Pdf" or Temp[1] == "PDF":
+            try:
+                print("This is PDF" , nooo)
+                with open(i,'rb') as pdf_file:
+                    read_pdf = PyPDF2.PdfFileReader(pdf_file)
+                    # page = read_pdf.getPage(0)
+                    # page_content = page.extractText()
+                    # Resumes.append(Temp_pdf)
+
+                    number_of_pages = read_pdf.getNumPages()
+                    for page_number in range(number_of_pages): 
+
+                        page = read_pdf.getPage(page_number)
+                        page_content = page.extractText()
+                        page_content = page_content.replace('\n', ' ')
+                        # page_content.replace("\r", "")
+                        Temp_pdf = str(Temp_pdf) + str(page_content)
+                        # Temp_pdf.append(page_content)
+                        # print(Temp_pdf)
+                    Resumes.extend([Temp_pdf])
+                    Temp_pdf = ''
+                    # f = open(str(i)+str("+") , 'w')
+                    # f.write(page_content)
+                    # f.close()
+            except Exception as e: print(e)
+        if Temp[1] == "doc" or Temp[1] == "Doc" or Temp[1] == "DOC":
+            print("This is DOC" , i)
+                
+            try:
+                a = textract.process(i)
+                a = a.replace(b'\n',  b' ')
+                a = a.replace(b'\r',  b' ')
+                b = str(a)
+                c = [b]
+                Resumes.extend(c)
+            except Exception as e: print(e)
+                
+                
+        if Temp[1] == "docx" or Temp[1] == "Docx" or Temp[1] == "DOCX":
+            print("This is DOCX" , i)
+            try:
+                a = textract.process(i)
+                a = a.replace(b'\n',  b' ')
+                a = a.replace(b'\r',  b' ')
+                b = str(a)
+                c = [b]
+                Resumes.extend(c)
+            except Exception as e: print(e)
+                    
+                
+        if Temp[1] == "ex" or Temp[1] == "Exe" or Temp[1] == "EXE":
+            print("This is EXE" , i)
+            pass
+
+    print("Done Parsing.")
+    print("############ Resumes dataframe #############")
+    df = pd.DataFrame(list(zip(LIST_OF_FILES, Resumes)),columns =['File_name', 'Resume'])
+    print(df)
+    df.to_excel('resumes20200911.xls',index=False,header=True)
